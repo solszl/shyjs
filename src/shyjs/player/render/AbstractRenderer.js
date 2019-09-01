ac65bca76876ad5301f4b5a0181683f787c69c85
zhenliang.sun <solszl@163.com>
2019-09-02 00:23:33 +0800

    add basic method

--- src/shyjs/player/render/AbstractRenderer.js
+++ src/shyjs/player/render/AbstractRenderer.js
@@ -1,7 +1,7 @@
 /**
  *
  * Created Date: 2019-08-21, 23:19:11 (zhenliang.sun)
- * Last Modified: 2019-08-21, 23:23:00 (zhenliang.sun)
+ * Last Modified: 2019-09-02, 00:22:10 (zhenliang.sun)
  * Email: zhenliang.sun@gmail.com
  *
  * Distributed under the MIT license. See LICENSE file for details.
@@ -17,7 +17,22 @@
  * @author zhenliang.sun
  */
 export default class AbstractRender {
-  constructor() {}
+  constructor(width = 0, height = 0) {
+    this._width = width
+    this._height = height
+    this.context = null
+  }
 
   render(displayObject) {}
+  resize(width, height) {}
+  clear() {}
+  destroy() {}
+
+  get height() {
+    return 0
+  }
+
+  get width() {
+    return 0
+  }
 }
--- src/shyjs/player/render/canvas/CanvasRenderer.js
+++ src/shyjs/player/render/canvas/CanvasRenderer.js
@@ -3,7 +3,7 @@ import AbstractRenderer from '../AbstractRenderer'
 /**
  *
  * Created Date: 2019-08-21, 23:20:08 (zhenliang.sun)
- * Last Modified: 2019-08-21, 23:23:20 (zhenliang.sun)
+ * Last Modified: 2019-09-02, 00:22:58 (zhenliang.sun)
  * Email: zhenliang.sun@gmail.com
  *
  * Distributed under the MIT license. See LICENSE file for details.
@@ -19,11 +19,52 @@ import AbstractRenderer from '../AbstractRenderer'
  * @author zhenliang.sun
  */
 export default class CanvasRenderer extends AbstractRenderer {
-  constructor() {
-    super()
+  constructor(width, height) {
+    super(width, height)
+    // create canvas
+    this.canvas = this._createCanvas(width, height)
   }
 
   render(displayObject) {
     super.render(displayObject)
+  }
+
+  resize(width, height) {
+    super.resize(width, height)
+  }
+
+  clear() {
+    super.clear()
+  }
+
+  destroy() {
+    super.destroy()
+  }
+
+  get width() {
+    return this.canvas.width
+  }
+
+  get height() {
+    return this.canvas.height
+  }
+
+  /**
+   * 创建一个canvas用于渲染
+   *
+   * @param {*} width
+   * @param {*} height
+   * @returns
+   * @memberof CanvasRenderer
+   */
+  _createCanvas(width, height) {
+    let canvas = document.createElement('canvas')
+    if(!isNaN(width) && !isNaN(height)) {
+      canvas.width = width
+      canvas.height = height
+    }
+
+    this.context = canvas.getContext('2d')
+    return canvas
   }
 }
